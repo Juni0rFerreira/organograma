2b23bd55198823f24892024a288a27d128746d81
Juni0rFerreira <junioredilsonferreira@hotmail.com>
2023-08-28 17:35:12 -0300

    commit

---
+++ src/components/Banner/banner.js
@@ -0,0 +1,12 @@
+import './banner.css';
+
+function Banner() {
+    return (
+        <header className='banner'>
+            <img src="/assets/img/banner.png" alt="Imagem do Banner"/>
+        </header>
+    )
+
+}
+
+export default Banner
